78ade382da8baedc2b3e32185e955928d8820a85
trueaddict <50746660+trueaddict@users.noreply.github.com>
2021-11-16 19:23:07 +0200

    demo3 kuva

--- demo3.R
+++ demo3.R
@@ -2,3 +2,12 @@ values = table(
   c(0,1,2,3,4,5,6, 7, 8, 9),
   c(0,0,0,0,0,1,8,29,37,19)
 )
+median(values)
+mean(values)
+
+joukkue = 12
+kentta = 8
+
+a = factorial(joukkue) / factorial(joukkue - kentta)
+
+b = choose(joukkue, kentta)
